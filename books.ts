c888dbb084636f086d67a55f25dd3460c5df28cb
Ferran Bals - iMac <ferranbals@gmail.com>
2024-04-03 11:30:58 +0200

    Retrieve 100 books, added book example and storing more fields

--- books.ts
+++ books.ts
@@ -1,11 +1,16 @@
 export class Book {
   constructor(
-    public title: "string",
-    public author_name: string,
-    public publish_date: number,
-    public first_publish_year: number,
-    public number_of_pages_median: number,
-    public cover_i: string
+    public author_name: string, //23
+    public cover_edition_key: string, //38
+    public cover_i: string, //39
+    public edition_count: number, //43
+    public first_publish_year: number, //151
+    public first_sentence: string, //152
+    public key: string, //410
+    public lending_identifier_s: number, //451
+    public number_of_pages_median: number, //452
+    public title: "string", //855
+    public ratings_average: number //1167
   ) {}
 }
 
@@ -17,23 +22,45 @@ export const loadBooks = async (n: number) => {
   const { docs } = (await response.json()) as { docs: any[] };
 
   const books: Array<Book> = [];
+
+  //Almacenaremos el número de libros indicado en la llamada.
   let bookCounter = 0;
   for (const {
-    title,
     author_name,
-    first_publish_year,
-    number_of_pages_median,
-    publish_date,
+    cover_edition_key,
     cover_i,
+    edition_count,
+    first_publish_year,
+    first_sentence,
+    key,
+    lending_identifier_s,
+    number_of_pages_median,
+    title,
+    ratings_average,
   } of docs) {
+    //Almacenamos el primer elemento de los arrays de autores y primera fras  del libro
+    const author = Array.isArray(author_name) ? author_name[0] : author_name;
+    const first = Array.isArray(first_sentence)
+      ? first_sentence[0]
+      : first_sentence;
+
+    //Eliminamos "isbn_" del inicio del id
+    const id = lending_identifier_s?.startsWith("isbn_")
+      ? lending_identifier_s.substring(5)
+      : lending_identifier_s;
     books.push(
       new Book(
-        title,
-        author_name,
+        author,
+        cover_edition_key,
+        cover_i,
+        edition_count,
         first_publish_year,
+        first,
+        key,
+        id,
         number_of_pages_median,
-        publish_date,
-        cover_i
+        title,
+        ratings_average
       )
     );
     bookCounter++;
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { loadBooks } from "./books.js";
 
-const books = await loadBooks(50);
+const books = await loadBooks(100);
 
 console.log("My books");
 console.log(books);
