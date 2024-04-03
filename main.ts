089d91e798378d885298f4a25c5290319a283996
Ferran Bals - iMac <ferranbals@gmail.com>
2024-04-03 12:00:30 +0200

    Generating main HTML doc, empty

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
+import { writeFile } from "fs/promises";
+
 import { render } from "./render.js";
 import { loadBooks } from "./books.js";
 
 const books = await loadBooks(100);
 const html = render(books);
+await writeFile("books.html", html);
 
 console.log("My books");
 console.log(books);
--- render.ts
+++ render.ts
@@ -1,5 +1,21 @@
 import { Book } from "./books.js";
 
+const head = (title: string) => `
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>${title}</title>
+        <style>
+        </style>
+    </head>`;
+
 export const render = (books: Array<Book>) => {
-  return "Hello";
+  return `
+    <html>
+      ${head("Books List")}
+      <body>
+      <p>Lorem Ipsum</p>
+      </body>
+    </html>`;
 };
