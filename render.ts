357686040e1b953abddf82bf80da6f2f8f5325e5
Ferran Bals - iMac <ferranbals@gmail.com>
2024-04-03 16:16:02 +0200

    Book data displayed on its page + favicon

--- render.ts
+++ render.ts
@@ -2,13 +2,23 @@ import { Book } from "./books.js";
 import { writeFile } from "fs/promises";
 import fs from "fs";
 
-const head = (title: string) => `
+function generateHead(title: string) {
+  return `
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/x-icon" href="/img/bookIcon.png" />
         <title>${title}</title>
         <style>
+        a,
+        a:visited,
+        a:hover,
+        a:active {
+            text-decoration: underline;
+            user-select: none;
+            color: #1e1afb;
+}
         .book{
             outline: black 1px solid;
             margin: 1em;
@@ -17,19 +27,25 @@ const head = (title: string) => `
             flex-direction: row;
             align-items: center;
             margin: 1em;
-            cursor:pointer
 
         }
         .bookCover{
             width: 5rem;
         }
+        .bookPageCover{
+            width: 10rem;
+        }
         .bookInfo{
             margin: 1em
         }
+        .bold{
+            font-weight: bold;
+        }
         </style>
     </head>`;
+}
 
-const bookFolder = "./books";
+const bookFolder = "./books/";
 function createFolder(bookFolder: string) {
   if (!fs.existsSync(bookFolder)) {
     fs.mkdirSync(bookFolder);
@@ -42,29 +58,68 @@ function renderBooks(books: Array<Book>) {
     const book = books[i];
     html += `
     <div class="book">
-        <img class ="bookCover" src="${
-          book.cover_i ? book.cover_i : "Portada no disponible"
-        }" />
+    <img class ="bookCover" src="${
+      book.cover_i ? book.cover_i : "Portada no disponible"
+    }" />
         <div class="bookInfo">
             <h1>${i + 1}. ${book.title}</h1>
             <h2>${
               book.author_name ? book.author_name : "Autor no especificado"
             }</h2>
             <a href="/books/${book.edition_key}.html" 
-            onclick="${renderBook(book.edition_key)}"
+            onclick="${renderBook(book)}"
             >Más información</a>
         </div>
+
         
   </div>`;
   }
   return html;
 }
 
-async function renderBook(key: string) {
+async function renderBook(book: Book) {
   createFolder(bookFolder);
-  const html = ` <p>Test page</p>          `;
 
-  const filename = bookFolder + key + ".html";
+  const html = ` 
+    <html>
+        ${generateHead(book.title)}
+        <body>
+        <div>
+            <a href="/books.html">Volver a la lista</a>
+        </div>
+            <img class ="bookPageCover" src="${
+              book.cover_i ? book.cover_i : "Portada no disponible"
+            }" />
+            <p><span class="bold">Autor:</span> ${
+              book.author_name ? book.author_name : "Autor no especificado"
+            }</p>
+            <p><span class="bold">Título:</span> ${
+              book.title ? book.title : "Título no disponible."
+            }</p>
+            <p><span class="bold">Año de publicación:</span> ${
+              book.first_publish_year
+                ? book.first_publish_year
+                : "Información no disponible."
+            }</p>
+            <p><span class="bold">Número de páginas (aprox):</span> ${
+              book.number_of_pages_median
+                ? book.number_of_pages_median
+                : "Información no disponible."
+            }</p>
+            <p><span class="bold">Valoración media:</span> ${
+              book.ratings_average
+                ? book.ratings_average
+                : "Información no disponible."
+            }</p>
+            <p><span class="bold">Primera frase:</span> ${
+              book.first_sentence
+                ? book.first_sentence
+                : "Información no disponible."
+            }</p>
+            </body>
+        </html>`;
+
+  const filename = bookFolder + book.edition_key + ".html";
 
   await writeFile(filename, html);
 }
@@ -72,7 +127,8 @@ async function renderBook(key: string) {
 export const render = (books: Array<Book>) => {
   return `
     <html>
-      ${head("Books List")}
+    ${generateHead("Books List")}
+      
       <body>
         <p>${renderBooks(books)}</p>
       </body>
