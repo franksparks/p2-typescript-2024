f12d04460506ab55fa494d36d9180839497d7456
Ferran Bals - iMac <ferranbals@gmail.com>
2024-04-05 09:35:43 +0200

    Centered book entries

--- render.ts
+++ render.ts
@@ -14,63 +14,62 @@ function generateHead(title: string) {
         <link href="https://fonts.googleapis.com/css2?family=Special+Elite&display=swap" rel="stylesheet">
         <title>${title}</title>
         
-
         <style>
-        body{
+          body{
             background-color: #6495ED;
             font-family: "Special Elite", system-ui;
-        }
-        a,
-        a:visited,
-        a:hover,
-        a:active {
+          }
+          a,
+          a:visited,
+          a:hover,
+          a:active {
             text-decoration: none;
             user-select: none;
             cursor:pointer;
             color: black;
-        }
-        .book{
+          }
+          .book{
             background-color: white;
             outline: black 1px solid;
-            margin: 2em 1em;
+            margin: 2em auto;
             padding: 0.5em;
+            width: 80em;
             display: flex;
             flex-direction: row;
             align-items: center;
-        }
-        .hover:hover{
-          background-color: #D3D3D3;
-          box-shadow: 0.75em 1.5em;
-        }
-        .bookCoverMini{
+            justify-content:center;
+          }
+          .record{
+
+          }
+          .bookCoverMini{
             width: 5rem;
-        }
-        .bookCoverDetails{
+          }
+          .bookCoverDetails{
             width: 10rem;
-        }
-        .bookPreview{
+          }
+          .bookPreview{
             margin: 1em
-        }
-        .bookInfo{
-            background-color:white;
+          }
+          .bookInfo{
             margin: 1em
-        }
-        .bookDetails{
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-          background-color: white;
-            outline: black 1px solid;
-        }
-        .bold{
+          }
+          .hover:hover{
+            background-color: #D3D3D3;
+            box-shadow: 0.75em 1.5em;
+          }
+          .bold{
             font-weight: bold;
-        }
-        .italic{
-          font-style: italic;
-      }
-      h1, h2, .bookDetails h3{
-        font-weight: normal;
-      }
+          }
+          .under{
+            text-decoration: underline;
+          }
+          .italic{
+            font-style: italic;
+          }
+          h1, h2, .bookInfo h3{
+            font-weight: normal;
+          }
         </style>
     </head>`;
 }
@@ -111,34 +110,35 @@ async function renderBook(book: Book) {
     <html>
         ${generateHead(book.title)}
         <body>
-        <h1>Detalles del libro seleccionado</h1>
-        <div class="bookDetails">
+        <h1>Libros de la API de Open Library</h1>
+        <h2>Detalles del libro seleccionado</h2>
+        <div class="book record">
           <img class ="bookCoverDetails" src="${
             book.cover_i ? book.cover_i : "Portada no disponible"
           }" />
           <div class="bookInfo">
-            <h1><span class="bold">Título:</span> ${
+            <h1><span class="bold under">Título:</span> ${
               book.title ? book.title : "Título no disponible."
             }</h1>
-            <h2><span class="bold">Autor:</span> ${
+            <h2><span class="bold under">Autor:</span> ${
               book.author_name ? book.author_name : "Autor no especificado"
             }</h2>
-            <h3><span class="bold">Año de publicación:</span> ${
+            <h3><span class="under">Año de publicación:</span> ${
               book.first_publish_year
                 ? book.first_publish_year
                 : "Información no disponible."
             }</h3>
-            <h3><span class="bold">Número de páginas (aprox):</span> ${
+            <h3><span class="under">Número de páginas (aprox):</span> ${
               book.number_of_pages_median
                 ? book.number_of_pages_median
                 : "Información no disponible."
             }</h3>
-            <h3><span class="bold">Valoración media:</span> ${
+            <h3><span class="under">Valoración media:</span> ${
               book.ratings_average
                 ? book.ratings_average
                 : "Información no disponible."
             }</h3>
-            <h3><span class="bold">Primera frase:</span><span class="italic"> ${
+            <h3><span class="under">Primera frase:</span><span class="italic"> ${
               book.first_sentence
                 ? book.first_sentence
                 : "Información no disponible."
@@ -161,7 +161,7 @@ export const render = (books: Array<Book>) => {
     <html>
     ${generateHead("Books List")}
     <h1>Libros de la API de Open Library</h1>
-    <h3>Selecciona un libro para obtener más información</h3>
+    <h2>Selecciona un libro para obtener más información</h2>
       
       <body>
         <div>${renderBooks(books)}</div>
